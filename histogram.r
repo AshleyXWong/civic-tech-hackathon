d10cc05ce9594f2ef47035adbcd1d1837b14e949
hasaansoomro <hasaan.soomro@mail.utoronto.ca>
2018-07-23 23:04:05 -0400

    Created histogram of infraction description using R

---
+++ histogram.r
@@ -0,0 +1,10 @@
+d = read.csv('Parking_Tags_Data_2016_1.csv')
+d = count(data, 'infraction_description')
+d = d[order(d$freq, decreasing=TRUE), ]
+d = head(d, n = 52)
+descriptions = head(d$infraction_description, n = 52)
+frequency = head(d$freq, n = 52)
+ggplot(d, aes(descriptions, frequency)) +
+    geom_bar(stat = 'identity') +
+    ggtitle('Histogram of Infraction Description') +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
